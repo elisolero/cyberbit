a79e3ea3efeaaea5958225e8c750ff54b8a5eb81
enaim <eli@joomi.co.il>
2017-07-17 20:21:38 +0300

    fill components data 3

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {DeviceService} from '../device.service';
+declare var $: any;
 
 @Component({
   selector: 'app-accordion',
@@ -19,6 +20,51 @@ export class AccordionComponent implements OnInit {
 
   ngOnInit() {
     this.data =   this.deviceService.getDevices()[0].device_groups;
-  }
 
+      $(document).ready(function(){
+       $('.innerItems.checkList').change(function(){
+         var id = 'summery_' + $(this).attr('id');
+
+          if($(this).is(':checked')){
+            $('#'+id).show();
+         }else{
+           $('#'+id).hide();
+         }
+       })
+
+       $('.groupItem').change(function(){
+          if($(this).is(':checked')){
+            $(this).closest('.mainPanel').find(".innerItems").each((ind,val)=>{
+                 var id = 'summery_' + $(val).attr('id');
+                  $('#'+id).show();
+                  $(val).prop( "checked", true );
+            })
+          }else{
+            $(this).closest('.mainPanel').find(".innerItems").each((ind,val)=>{
+                  var id = 'summery_' + $(val).attr('id');
+                  $('#'+id).hide();
+                $(val).prop( "checked", false );
+            })
+          }
+       });
+
+
+    $('.select_protocol').each(function(){
+        var name = $(this).attr('name');
+        var id = $(this).attr('id');
+        $('.summery_protocol').append('<li style="display:none;" id="summery_'+id+'" >'+name+'</li>')
+    })
+       $(document).on('change','.select_protocol',function(){
+            var id = $(this).attr('id');
+        if($(this).is(':checked')){
+            $('#summery_'+id).show();
+         }else{
+           $('#summery_'+id).hide();
+         }
+
+        });
+
+
+      })
+  }
 }
