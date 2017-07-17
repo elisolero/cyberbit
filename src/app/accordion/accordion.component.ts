a2e2acb3c95a1c68b96bb72f2d2c7a29361d2fce
enaim <eli@joomi.co.il>
2017-07-18 00:09:32 +0300

    fill components data 4

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -22,6 +22,7 @@ export class AccordionComponent implements OnInit {
     this.data =   this.deviceService.getDevices()[0].device_groups;
 
       $(document).ready(function(){
+        
        $('.innerItems.checkList').change(function(){
          var id = 'summery_' + $(this).attr('id');
 
@@ -66,14 +67,14 @@ export class AccordionComponent implements OnInit {
       }
     });
 
-    //Append Protocol Items for Summery
+    //Append Time Items for Summery
     $('.select_time').each(function(){
         var name = $(this).attr('name');
         var id = $(this).attr('id');
         $('.summery_time').append('<li style="display:none;" id="summery_'+id+'" >'+name+'</li>')
     });
 
-    //On change Protocol Items for Summery
+    //On change Time Items for Summery
     $(document).on('change','.select_time',function(){
 
       var id = $(this).attr('id');
@@ -84,6 +85,53 @@ export class AccordionComponent implements OnInit {
       }
     });
 
+    $('#clearData').click(()=>{
+        $("[type=checkbox]").each((ind,val)=>{
+         if($(val).is(':checked'))
+              $(val).click();
+        });
+    })
+
+
+var str;
+
+var strDevice = '';
+var strProtocol = '';
+var strTime = '';
+  
+  //onClick get all params and submit
+  $('#startLearning').click((e)=>{
+    e.preventDefault();
+    strDevice += '?devices=';
+
+      $('.innerItems').each((ind,val)=>{
+            if($(val).is(":checked")){
+              strDevice += $(val).val() + ',';
+            }
+      }).promise().done(()=>{
+          strProtocol += '&protocols=';
+          $('.select_protocol').each((ind2,val2)=>{
+            if($(val2).is(":checked")){
+              strProtocol += $(val2).val() + ',';
+            }
+          }).promise().done( ()=>{
+            $('.select_time').each((ind3,val3)=>{
+             strTime = '&times=';
+              if($(val3).is(":checked")){
+                  strTime += $(val3).val() + ',';
+              }
+            });
+          }).promise().done(()=>{
+            var pathArray = location.href.split( '/' );
+            var protocol = pathArray[0];
+            var host = pathArray[2];
+            var url = protocol + '//' + host+strDevice+strProtocol+strTime;
+
+            window.location.href =url;
+            // console.log(strDevice,strProtocol,strTime)
+          });
+      });      
+  })
 
       })
   }
