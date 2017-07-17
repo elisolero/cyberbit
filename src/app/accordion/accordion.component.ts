1e3bd5f83a8f4c996664226e618bbbc6d1751570
enaim <eli@joomi.co.il>
2017-07-17 05:40:22 +0300

    add service

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {DeviceService} from '../device.service';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrls: ['./accordion.component.css']
+  styleUrls: ['./accordion.component.css'],
+  providers: [DeviceService]
 })
 export class AccordionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private deviceService: DeviceService) {
+  
+   }
 
   ngOnInit() {
+    return this.deviceService.devices;
   }
 
 }
